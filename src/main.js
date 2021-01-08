dbdaaee1438f299b563a87ff8fb289205f82fdbe
anetz89 <anetz@gmx.net>
2021-01-09 00:55:10 +0100

    added mor patterns and first options

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
+var currentTileSize;
+var currentTileList;
+updateTileSet('WINGED');
+
 var map = L.map('map', {
 	zoomControl: false
-}).setView([51.505, -0.09], 13);
-
-mapLink = 
-    '<a href="https://christophercarlson.com/portfolio/multi-scale-truchet-patterns/">Christopher Carlson</a>';
-
+}).setView([51.505, -0.09], 10);
 
 L.TileLayer.TruchetPattern = L.TileLayer.extend({
     getTileUrl: function(coords) {
-        var i = Math.ceil( Math.random() * 9 );
-        return "./tiles/0" + i + ".png";
+        var i = Math.floor( Math.random() * currentTileList.length );
+        return this._url + "/" + currentTileList[i] + ".png";
+    },
+    setTileSize: function(size) {
+    	this.options.tileSize = L.point(size, size);
+    	this.redraw();
     }
 });
 
@@ -17,9 +21,30 @@ L.TileLayer.truchetPattern = function() {
 	return new L.TileLayer.TruchetPattern();
 }
 
-new L.TileLayer.TruchetPattern('./tiles/', {
-    attribution: 'Inspired by ' + mapLink,
-    maxZoom: 13,
-    minZoom: 13,
-    tileSize: 74
-}).addTo(map);
+var truchetPattern = new L.TileLayer.TruchetPattern('./tiles/winged', {
+    attribution: 'Inspired by <a href="https://christophercarlson.com/portfolio/multi-scale-truchet-patterns/">Christopher Carlson</a>',
+    maxZoom: 10,  // fake; 4
+    minZoom: 10,
+    tileSize: currentTileSize
+});
+
+truchetPattern.addTo(map);
+
+function updateTileSize() {
+    truchetPattern.setTileSize(parseFloat(zoomSelect.value) * currentTileSize);
+}
+function updateTileSet(tileSet) {
+	currentTileSize = CONFIG.TILESETS[tileSet].tileSize;
+	currentTileList = CONFIG.TILESETS[tileSet].tileList;
+}
+
+var zoomSelect = document.getElementById('zoom');
+zoomSelect.onchange = updateTileSize;
+
+
+var tilesetSelect = document.getElementById('tileset');
+tilesetSelect.onchange = function () {
+	updateTileSet(tilesetSelect.value.toUpperCase());
+    truchetPattern.setUrl('./tiles/' + tilesetSelect.value, false);
+    updateTileSize();
+}
